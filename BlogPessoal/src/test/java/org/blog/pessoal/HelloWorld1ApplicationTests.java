772689c3694d4c331a6bc7433d91a9b9fdc038f5
Isaque Costa <78651671+Isaquesc@users.noreply.github.com>
2021-05-26 23:33:09 -0300

    #09 - Atividade CRUD Back-end blog-pessoal 1.

---
+++ BlogPessoal/src/test/java/org/blog/pessoal/HelloWorld1ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.blog.pessoal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorld1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
